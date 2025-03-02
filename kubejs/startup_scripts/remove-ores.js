5a3ddcb37c86f876d89f9f2d4ec926f8294d7c86
Nadwey <nadweyyy@gmail.com>
2025-03-03 00:05:22 +0100

    feat: More ore unifying

--- kubejs/startup_scripts/remove-ores.js
+++ kubejs/startup_scripts/remove-ores.js
@@ -1,11 +1,30 @@
-// TODO
+// TODO: Finish this and maybe do it "the proper way"
 WorldgenEvents.remove((event) => {
   event.removeOres((props) => {
     props.worldgenLayer = "underground_ores";
     props.blocks = [
       // Immersive Engineering
-      "immersiveengineering:deepslate_ore_uranium",
       "immersiveengineering:ore_uranium",
+      "immersiveengineering:deepslate_ore_uranium",
+      "immersiveengineering:ore_silver",
+      "immersiveengineering:deepslate_ore_silver",
+
+      // Create
+      "create:zinc_ore",
+      "create:deepslate_zinc_ore",
+
+      // Railcraft
+      "railcraft:nickel_ore",
+      "railcraft:deepslate_nickel_ore",
+      "railcraft:tin_ore",
+      "railcraft:deepslate_tin_ore",
+      "railcraft:zinc_ore",
+      "railcraft:deepslate_zinc_ore",
+
+      // XyCraft: World
+      "xycraft_world:aluminum_ore_stone",
+      "xycraft_world:aluminum_ore_deepslate",
+      "xycraft_world:aluminum_ore_kivi",
     ];
   });
 });
